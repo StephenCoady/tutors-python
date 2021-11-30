d5fd10787c5bc51fc212d12c41db95a75e4936a5
Ben <BenCapper19@gmail.com>
2021-11-30 19:30:35 +0000

    getDirectories test
    - Checks the root directory, filtering out directories
      then returns a list of files
    - Changed how cleanup is done on all tests
    
    Signed-off-by: Ben <BenCapper19@gmail.com>

--- tpython/main.py
+++ tpython/main.py
@@ -1,6 +1,7 @@
-from tests.utils.futils import test_read_file, test_write_file, test_get_image, test_get_parent_folder
+from tests.utils.futils import test_write_file, test_read_file, test_get_image, test_get_parent_folder, test_get_directories
 
 test_write_file()
 test_read_file()
 test_get_image()
 test_get_parent_folder()
+test_get_directories()
--- tpython/tests/utils/futils.py
+++ tpython/tests/utils/futils.py
@@ -1,15 +1,21 @@
-from lib.utils.futils import getParentFolder, writeFile, readFile, getImageFile
+from genericpath import isfile
+from lib.utils.futils import getParentFolder, writeFile, readFile, getImageFile, getDirectories
 import os
 
 folder = "tests"
 file = "futil_test_file"
+mdfile = "test_md_file.md"
 path = folder + '/' + file
+mdpath = folder + '/' + mdfile
 img_file = "image.jpg"
 
 
 def test_write_file():
-    os.remove(path)
-    os.rmdir(folder)
+    try:
+        os.remove(path)
+        os.rmdir(folder)
+    except:
+        print("Doesn't exist")
     contents = "i am the content"
     writeFile(folder,file,contents)
     assert os.path.exists(path)
@@ -18,16 +24,21 @@ def test_write_file():
     assert contents == read_file
 
 def test_read_file():
-    os.remove(path)
-    os.rmdir(folder)
-    list_of_lines = []
+    try:
+        os.remove(path)
+        os.rmdir(folder)
+    except:
+        print("Doesn't exist")
     contents = "i am the content \r\n and this is a line with a line terminator for mac"
     writeFile(folder, file, contents)
     new_file = readFile(path)
     assert '\r' not in new_file
 
 def test_get_image():
-    remove_test_dir()
+    try:
+        os.remove("image.jpg")
+    except:
+        print("Doesn't exist")
     os.mknod("image.jpg")
     test_img = getImageFile("image")
     assert test_img == "image.jpg"
@@ -35,3 +46,19 @@ def test_get_image():
 def test_get_parent_folder():
     parent = getParentFolder()
     assert parent == "src" # This must be changed to your parent folder to pass
+
+def test_get_directories():
+    try:
+        os.remove(mdpath)
+        os.rmdir(folder)
+    except:
+        print("Doesn't exist")
+    src = "."
+    writeFile(src, mdfile, "test")
+
+    directories = []
+    directories = getDirectories(src)
+    for item in directories:
+        assert isfile(item)
+
+    
