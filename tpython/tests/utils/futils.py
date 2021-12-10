9dcb40dc6b803c4da27c682f8c98c4ded10b3d14
Ben <BenCapper19@gmail.com>
2021-12-10 13:39:44 +0000

    File formatting
    - Ran black on the futils test file
    
    Signed-off-by: Ben <BenCapper19@gmail.com>

--- tpython/tests/utils/futils.py
+++ tpython/tests/utils/futils.py
@@ -1,24 +1,33 @@
-from genericpath import isdir, isfile
-from lib.utils.futils import copyFileToFolder, copyFolder, getParentFolder, verifyFolder, writeFile, readFile, getImageFile, getDirectories
+from genericpath import isdir
+from lib.utils.futils import (
+    copyFileToFolder,
+    copyFolder,
+    getParentFolder,
+    verifyFolder,
+    writeFile,
+    readFile,
+    getImageFile,
+    getDirectories,
+)
 import os
 
 
 folder = "tests"
 folders = "test2/subtest/subsubtest"
 file = "futil_test_file"
-path = folder + '/' + file
+path = folder + "/" + file
 img_file = "image.jpg"
 mdfile = "test_md_file.md"
-mdpath = folder + '/' + mdfile
+mdpath = folder + "/" + mdfile
 
 
 def test_write_file():
     contents = "i am the content"
-    writeFile(folder,file,contents)
+    writeFile(folder, file, contents)
     assert os.path.exists(path)
     opened_file = open(path, "r")
     read_file = opened_file.read()
-    assert contents == read_file 
+    assert contents == read_file
     os.remove(path)
     os.rmdir(folder)
 
@@ -27,9 +36,10 @@ def test_read_file():
     contents = "i am the content \r\n and this is a line with a line terminator for mac"
     writeFile(folder, file, contents)
     new_file = readFile(path)
-    assert '\r' not in new_file
+    assert "\r" not in new_file
     os.remove(path)
     os.rmdir(folder)
+
 
 def test_get_image():
     os.mknod("image.jpg")
@@ -37,9 +47,11 @@ def test_get_image():
     assert test_img == "image.jpg"
     os.remove("image.jpg")
 
+
 def test_get_parent_folder():
     parent = getParentFolder()
-    assert parent == "src" # This must be changed to your parent folder to pass
+    assert parent == "src"  # This must be changed to your parent folder to pass
+
 
 def test_get_directories():
     src = "."
@@ -57,6 +69,7 @@ def test_verify_folder():
     assert os.path.exists(folders)
     os.removedirs(folders)
 
+
 def test_copy_file_to_folder():
     folder_path = "tests3/test4"
     assert not os.path.exists(folder_path)
@@ -67,8 +80,9 @@ def test_copy_file_to_folder():
     copyFileToFolder(img_file, folder_path)
     assert os.path.exists(folder_path + "/" + img_file)
     os.remove(img_file)
-    os.remove(folder_path+ "/" + img_file)
+    os.remove(folder_path + "/" + img_file)
     os.removedirs(folder_path)
+
 
 def test_copy_folder():
     src = "src"
